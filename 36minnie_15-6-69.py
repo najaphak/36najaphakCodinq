a21f3ef3f012063a17679227980b75a692429dc0
najaphak <27084@bbw.ac.th>
2026-06-15 09:47:16 +0700

    โปรแกรมเลขคู่หรือเลขคี่
    ได้เรียน print input เข้าใจขึ้นมาก
    ได้เรียนเกี่ยวกับ I P O ของโปรแกรมเลขคู่เลขคี่ เข้าใจอยู่ค่ะ

---
+++ 36minnie_15-6-69.py
@@ -0,0 +1,9 @@
+number = int(input("Enter your number: "))
+
+if number %2 == 0:
+    print("even number")
+else:
+    print("obd number")
+print("โปรแกรมเลขคู่หกรือเลขคี่")
+print("by minnie 4/4")
+print("thank you")

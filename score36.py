14fbaa46ffba1e7c9737d22f74847a84706c1757
najaphak <27084@bbw.ac.th>
2026-06-18 14:04:17 +0700

    โปรแกรมคำนวณคะแนนรวม
    36 มินนี่ ได้เขียนโปรแกรมคำนวรคะแนนรวมเข้าใจขึ้นสุดๆ รักครูเฟินค่ะ^^

---
+++ score36.py
@@ -0,0 +1,15 @@
+score1 = int(input("คะแนนวิชาที่ 1: "))
+score2 = int(input("คะแนนวิชาที่ 2: "))
+score3 = int(input("คะแนนวิชาที่ 3: "))
+             
+total_score = (score1 + score2 + score3)
+
+if total_score < 6:
+    print("ระดับคะแนน  ควรปรับปรุง")
+elif total_score < 79 :
+    print("ระดับระแนน = ผ่าน")
+else:
+    print("ระดับคะแนน = ดีเยี่ยม")
+
+    print("ระดับคะแนนของคุณ = ", total_score)
+    print("คะแนนเฉลี่ย 3 วิชา = ", total_score / 3)
